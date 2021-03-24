1a51c81e2951e3d787a00431d4b9f2b6abbf527e
ezekiel444 <50213769+ezekiel444@users.noreply.github.com>
2021-03-24 23:35:49 +0100

    contentful config setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,10 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+
+
+
 module.exports = {
   siteMetadata: {
     title: "coffee_seller",
@@ -14,6 +21,14 @@ module.exports = {
       },
       
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    }
    
   ],
   
